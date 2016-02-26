051e8bf4bb01f351804415e99603ca98b7c71146
xcw0579 <xcw0579@175game.com>
2016-02-26 16:26:42 +0800

    更新README.md，创建gitignore。

---
+++ modi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+
+find  . -type f -name '*.c' | xargs sed -i 's/.P1(\(.*\),\(.*\))/(\1 \2)/g'
+
+find  . -type f -name '*.c' | xargs sed -i 's/.P2(\(.*\),\(.*\),\(.*\),\(.*\))/(\1 \2,\3 \4)/g'
+
+find  . -type f -name '*.c' | xargs sed -i 's/.P3(\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\))/(\1 \2,\3 \4,\5 \6)/g'
+
+find  . -type f -name '*.c' | xargs sed -i 's/.P4(\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\))/(\1 \2,\3 \4,\5 \6,\7 \8)/g'
+
+
+# 去掉PROT
+find  . -type f -name '*.c' | xargs sed -i 's/PROT((\(.*\)))/(\1)/g'
+
